22d95294c604ac9fc1415712716ed2a33299bbbb
FosterToster <gribnoysalatik@gmail.com>
2021-10-29 23:38:22 +0700

    nested validators test

--- outpost/__main__.py
+++ outpost/__main__.py
@@ -15,31 +15,53 @@ class User:
     phone: Phone
 
 
-
-
-class UserValidator(Outpost, model=User):
-    def requirements(self):
-        return self.fields.id
-    ...
-
 class PhoneValidator(Outpost, model=Phone):
     def requirements(self):
         return self.fields.number
     ...
 
+
+class UserValidator(Outpost, model=User):
+    def requirements(self):
+        return self.fields.id
+
+    def validators(self, fieldvalidator):
+        
+        fieldvalidator(self.fields.phone)(PhoneValidator)
+
+        @fieldvalidator(self.fields.id)
+        def idvalidator(value):
+            return value
+
+
+
+    ...
+
+
 class CreateUserValidator(UserValidator):
     
-    # def requirements(self):
-    #     return AND(
-    #         self.fields.name,
-    #         self.fields.phone
-    #     )
+    def requirements(self):
+        return AND(
+            self.fields.name,
+            self.fields.phone,
+            OR(
+                self.fields.id,
+                self.fields.phone
+            ),
+            NOT(self.fields.hash)
+        
+        )
 
     def readonly(self):
         return {
             self.fields.hash: True,
             self.fields.id: False
         }
+
+    def validators(self, fieldvalidator):
+        @fieldvalidator(self.fields.name)
+        def namevalidator(v):
+            return v
 
     ...
         
