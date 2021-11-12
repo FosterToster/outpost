b1fff70a4a263b6e2212ac1422a7f33935ec8abf
Danila Matyshev <gribnoysalatik@gmail.com>
2021-11-12 18:57:53 +0700

    new examples

--- outpost/__main__.py
+++ outpost/__main__.py
@@ -24,14 +24,16 @@ class PhoneValidator(Outpost, model=Phone):
 
 
 class UserValidator(Outpost, model=User):
+    
+
     def requirements(self):
         return self.fields.id
 
-    def validators(self, fieldvalidator):
+    def validators(self, supervalidator):
         
-        fieldvalidator(self.fields.phone)(PhoneValidator)
+        supervalidator(self.fields.phone)(PhoneValidator)
 
-        @fieldvalidator(self.fields.id)
+        @supervalidator(self.fields.id)
         def idvalidator(value):
             return value
 
@@ -45,19 +47,34 @@ class CreateUserValidator(UserValidator):
     def requirements(self):
         return self.fields.hash
 
-    def readonly(self):
-        return {
-            self.fields.name: False
-        }
+    # def readonly(self):
+    #     return {
+    #         self.fields.name: False
+    #     }
 
-    def validators(self, fieldvalidator):
-        @fieldvalidator(self.fields.name)
+    def validators(self, supervalidator):
+        
+        @supervalidator(self.fields.name)
         def namevalidator(v):
             return v
 
     ...
         
 try:
-    print(CreateUserValidator.validate({'id': '1234', 'hash': "fafsfd", 'name': 'asdf'}))
+
+    dataset = {
+        'id': 1234, 
+        'hash': "fafsfd", 
+        # 'name': 'asdf'
+    }
+
+    print(CreateUserValidator.validate(dataset).dataset())
+    print(CreateUserValidator.context().defaults({CreateUserValidator.fields.name: "Federico Felini", CreateUserValidator.fields.phone: {PhoneValidator.fields.number: 12341234123}}).validate(dataset).dataset())
+
+    with CreateUserValidator.context() as context:
+        context.defaults({CreateUserValidator.fields.name: "Federico Felini", CreateUserValidator.fields.phone: {PhoneValidator.fields.number: 12341234123}})
+        context.validate(dataset)
+        print(context.dataset())
+
 except ValidationError as e:
     print(e.__class__.__name__, str(e))
